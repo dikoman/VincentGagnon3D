8e1fb3146170feed29db72fef1e0fcbbcf18a361
Vincent Gagnon <dikoman123@hotmail.com>
2019-02-03 03:25:19 -0500

    fzdf
    gdzfd

---
+++ js/portfolio.js
@@ -0,0 +1,123 @@
+$('.carousel').carousel();
+
+/*111111111111111*/
+$('.afficherProjet1').click(function() {
+  $('.Projet1').show();
+  $('.blackBackground1').show(); 
+});
+
+$('.GoBackButton').click(function() {
+  $('.Projet1').hide();
+  $('.blackBackground1').hide();
+});
+
+/*2222222222222222*/
+$('.afficherProjet2').click(function() {
+  $('.Projet2').show();
+  $('.blackBackground1').show(); 
+});
+
+$('.GoBackButton').click(function() {
+  $('.Projet2').hide();
+  $('.blackBackground1').hide();
+});
+
+
+/*33333333333333333*/
+$('.afficherProjet3').click(function() {
+  $('.Projet3').show();
+  $('.blackBackground1').show(); 
+});
+
+$('.GoBackButton').click(function() {
+  $('.Projet3').hide();
+  $('.blackBackground1').hide();
+});
+
+
+/*444444444444444444*/
+$('.afficherProjet4').click(function() {
+  $('.Projet4').show();
+  $('.blackBackground1').show(); 
+});
+
+$('.GoBackButton').click(function() {
+  $('.Projet4').hide();
+  $('.blackBackground1').hide();
+});
+
+
+/*555555555555555555*/
+$('.afficherProjet5').click(function() {
+  $('.Projet5').show();
+  $('.blackBackground1').show(); 
+});
+
+$('.GoBackButton').click(function() {
+  $('.Projet5').hide();
+  $('.blackBackground1').hide();
+});
+
+
+/*66666666666666666666*/
+$('.afficherProjet6').click(function() {
+  $('.Projet6').show();
+  $('.blackBackground1').show(); 
+});
+
+$('.GoBackButton').click(function() {
+  $('.Projet6').hide();
+  $('.blackBackground1').hide();
+});
+
+
+
+/*777777777777777777777*/
+$('.afficherProjet7').click(function() {
+  $('.Projet7').show();
+  $('.blackBackground1').show(); 
+});
+
+$('.GoBackButton').click(function() {
+  $('.Projet7').hide();
+  $('.blackBackground1').hide();
+});
+
+
+
+/*888888888888888888888*/
+$('.afficherProjet8').click(function() {
+  $('.Projet8').show();
+  $('.blackBackground1').show(); 
+});
+
+$('.GoBackButton').click(function() {
+  $('.Projet8').hide();
+  $('.blackBackground1').hide();
+});
+
+
+
+/*9999999999999999999999*/
+$('.afficherProjet9').click(function() {
+  $('.Projet9').show();
+  $('.blackBackground1').show(); 
+});
+
+$('.GoBackButton').click(function() {
+  $('.Projet9').hide();
+  $('.blackBackground1').hide();
+});
+
+
+
+/*10 10 10 10 10 10*/
+$('.afficherProjet10').click(function() {
+  $('.Projet10').show();
+  $('.blackBackground1').show(); 
+});
+
+$('.GoBackButton').click(function() {
+  $('.Projet10').hide();
+  $('.blackBackground1').hide();
+});
